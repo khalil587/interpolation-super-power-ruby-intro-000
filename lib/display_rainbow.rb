367138c9f633b181f6cdc3991c22f739b0982e67
khalil Albert lindsay <lindsay.khalil@gmail.com>
2021-02-23 23:51:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,4 +7,4 @@ I = ":indigo"
 V = ":violet"
 colors = ["R", "O", "Y", "G", "B", "I", "V"]
 def display_rainbow (colors)
-puts "R:#{colors[0], O: {colors[1]}  "
+puts "R:#{colors[0], O: {colors[1]}, "
