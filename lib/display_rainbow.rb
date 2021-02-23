f8c2ec05e18c5d3c64097b149713ba3349b91635
khalil Albert lindsay <lindsay.khalil@gmail.com>
2021-02-23 23:32:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
-# Write your #display_rainbow method here
+R = ":red"
+O = ":orange"
+Y = ":yellow"
+G = ":green"
+B = ":blue"
+I = ":indigo"
+V = ":violet"
+rainbow = ["R", "O", "Y", "G", "B", "I", "V"]
+def 
+display_rainbow
